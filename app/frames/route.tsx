f07f276128ce8b558eed10d956fc9df1f30ff538
Qedric <qedricjames@gmail.com>
2024-05-16 11:21:05 +0200

    Revert "test smaller initial frame"
    This reverts commit 3322fa3d77c1fd650b003caaf8de0cacc0098ffd.

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -1,13 +1,37 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next"
 import { frames } from "./frames"
+import buildings from "@/app/data/buildings.json"
 
 const handleRequest = frames(async (ctx) => {
 
     return {
         image: (
             <div tw="flex w-full h-full bg-gray-200 items-center justify-center shadow-2xl">
-                <h1>?</h1>
+                <div tw="flex flex-wrap mx-auto w-3/5 p-8 h-7/8 shadow-2xl bg-pink-700 text-white rounded-lg">
+                    <div tw="p-4 flex flex-col items-center justify-center w-full h-4/5 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                        <h1>?</h1>
+                    </div>
+                    <div tw="my-5 w-full flex justify-between items-center">
+                        <div tw="flex text-sm w-1/3 mr-2 p-2 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                            Liquidity
+                        </div>
+                        <div tw="flex text-sm w-1/3 mx-2 p-2 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                            Holders
+                        </div>
+                        <div tw="flex text-sm w-1/3 ml-2 p-2 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                            Supply
+                        </div>
+                    </div>
+                    <div tw="w-full flex justify-between items-center">
+                        <div tw="flex text-base w-2/5 mr-4 p-5 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                            Current Price
+                        </div>
+                        <div tw="flex text-base w-3/5 ml-4 p-5 bg-fuchsia-800 border border-yellow-400 rounded-lg">
+                            City
+                        </div>
+                    </div>
+                </div>
             </div>
         ),
         imageOptions: {
@@ -20,6 +44,9 @@ const handleRequest = frames(async (ctx) => {
             </Button>,
             <Button action="link" target={process.env.NEXT_PUBLIC_MORE_INFO_LINK as string}>
                 Farconic
+            </Button>,
+            <Button action="post" target={{ query: { building: JSON.stringify(buildings[Math.floor(Math.random() * buildings.length)]) }, pathname: "/building/card" }}>
+                Random
             </Button>,
             <Button action="link" target={process.env.NEXT_PUBLIC_MORE_INFO_LINK as string}>
                 My Cards / Learn more
