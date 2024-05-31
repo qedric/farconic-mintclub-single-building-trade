45b41ed029176547de71c129a952adf9081a81dc
Qedric <qedricjames@gmail.com>
2024-05-31 11:56:58 +0200

    card styling adjustments

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,8 +11,7 @@ export const CardImage = async ( building:NFT, userImg:string | undefined, userN
 
     //console.log('mintclub token details:', detail)
     //console.log('opensea data:', openseaData)
-
-    console.log('\n\nbuilding.building_color:', building.building_color)
+    //console.log('\n\nbuilding.building_color:', building.building_color)
 
     const buildingName = building.metadata.name
     let buildingNameFontSize:string = buildingName.length > 28 
@@ -20,33 +19,33 @@ export const CardImage = async ( building:NFT, userImg:string | undefined, userN
         : 'text-4xl my-4 pb-2'
 
     //const style:string = `flex text-4xl p-2 bg-[#e9464b] rounded-[20px] uppercase`
-    const style:string = `flex text-4xl p-2 bg-[${building.building_color}] rounded-[20px] uppercase`
+    const style:string = `flex text-4xl p-2 bg-[${building.building_color}] rounded-[16px] uppercase`
 
     return (
         <div tw="flex w-full h-full items-center justify-center">
-            <div tw="flex w-full h-full items-center justify-center" style={{ backgroundPosition: '-22px 0%', backgroundSize: '105% 100%', backgroundImage: `url(${process.env.NEXT_PUBLIC_GATEWAY_URL}/QmSC9TQbDfdtUQq6aTkQTiFGXs6faakwbRchBHL2oDomcS)`}}>
-            <div tw="flex flex-wrap relative p-6 w-3/5 h-11/12 text-white rounded-[40px]" >
-                <div tw={ `py-2 flex flex-col items-center justify-center w-full h-9/12 ${ style }` }>
-                    <p tw="text-2xl absolute top-4 left-9">{ building.metadata.attributes.find(attr => attr.trait_type == 'Country')?.value }</p>
-                    <p tw="text-2xl absolute top-4 right-9">{ building.metadata.attributes.find(attr => attr.trait_type == 'City')?.value }</p>
+            <div tw="flex w-full h-full items-center justify-center" style={{ backgroundPosition: '-22px 1%', backgroundSize: '105% 100%', backgroundImage: `url(${process.env.NEXT_PUBLIC_GATEWAY_URL}/QmSC9TQbDfdtUQq6aTkQTiFGXs6faakwbRchBHL2oDomcS)`}}>
+            <div tw="flex flex-wrap relative top-2.5 p-6 w-3/5 h-11/12 text-white rounded-[40px]" >
+                <div tw={ `relative py-2 flex flex-col items-center justify-center w-full h-9/12 ${ style }` }>
+                    <p tw="text-2xl absolute -top-2 left-9">{ building.metadata.attributes.find(attr => attr.trait_type == 'Country')?.value }</p>
+                    <p tw="text-2xl absolute -top-2 right-9">{ building.metadata.attributes.find(attr => attr.trait_type == 'City')?.value }</p>
                     <img tw="pt-16 w-full" style={{ objectFit: 'contain' }} src={ building.metadata.image.replace("ipfs://", `${process.env.NEXT_PUBLIC_GATEWAY_URL}`) as string } />
-                    <h3 tw={`px-8 text-center ${buildingNameFontSize}`}>{ buildingName }</h3>
+                    <h3 tw={`px-8 pt-4 text-center ${buildingNameFontSize}`}>{ buildingName }</h3>
                     { userImg && 
-                        <div tw="absolute -top-14 w-full flex flex-col justify-center items-center">
+                        <div tw="absolute -top-18 w-full flex flex-col justify-center items-center">
                             <img src={userImg} tw="w-28 h-28 rounded-full" />
-                            <div tw="flex lowercase mt-1" style={{ fontSize: '16px'}}>@{ userName }</div>
+                            <div tw="flex lowercase mt-1" style={{ fontSize: '20px'}}>@{ userName }</div>
                         </div>
                     }
                 </div>
-                <div tw="my-5 w-full flex justify-between items-center">
+                <div tw="my-4 w-full flex justify-between items-center">
                     <div tw="flex flex-col w-1/2">
-                        <div tw="px-2 text-2xl font-bold">Current Price:</div>
+                        <div tw="px-2 text-2xl font-bold mb-0.5">Current Price:</div>
                         <div tw={ `mr-2 ${ style }` }>
                             <span tw="px-2">{ Math.round(parseFloat(ethers.formatEther(detail.info.priceForNextMint))*1e4) / 1e4 }</span>ETH
                         </div>
                     </div>
                     <div tw="flex flex-col w-1/2">
-                        <div tw="px-2 text-2xl font-bold">Supply:</div>
+                        <div tw="px-2 text-2xl font-bold mb-0.5">Supply:</div>
                         <div tw={ `ml-2 ${ style }` }>
                             <span tw="px-2">{ detail.info.currentSupply.toString() }</span>
                         </div>
@@ -54,13 +53,13 @@ export const CardImage = async ( building:NFT, userImg:string | undefined, userN
                 </div>
                 <div tw="w-full flex justify-between items-center">
                     <div tw="flex flex-col w-1/2">
-                        <div tw="px-2 text-2xl font-bold">Liquidity:</div>
+                        <div tw="px-2 text-2xl font-bold mb-0.5">Liquidity:</div>
                         <div tw={ `mr-2 ${ style }` }>
                             <span tw="px-2">{ Math.round(parseFloat(ethers.formatEther(detail.info.reserveBalance))*1e4) / 1e4 }</span>ETH
                         </div>
                     </div>
                     <div tw="flex flex-col w-1/2">
-                        <div tw="px-2 text-2xl font-bold">Holders:</div>
+                        <div tw="px-2 text-2xl font-bold mb-0.5">Holders:</div>
                         <div tw={ `ml-2 ${ style }` }>
                             <span tw="px-2">{ openseaData.owners.length }</span>
                         </div>
