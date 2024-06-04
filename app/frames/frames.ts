e18c25eee55e3bc3e6cb5459bfd64ccee1311be9
Qedric <qedricjames@gmail.com>
2024-06-04 15:14:19 +0200

    added farcasterHub middleware to get user data

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,9 +1,11 @@
 import { createFrames } from "frames.js/next"
+import { farcasterHubContext } from "frames.js/middleware"
 import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker"
 
 export const frames = createFrames({
   basePath: "/frames",
   middleware: [
+    farcasterHubContext(),
     imagesWorkerMiddleware({
       imagesRoute: "/images"
     }),
