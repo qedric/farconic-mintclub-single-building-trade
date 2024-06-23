94cd8f953a30ce38498b4097e867c0fe3f7b512f
Qedric <qedricjames@gmail.com>
2024-06-23 22:43:30 +0200

    added airstack middleware for frame validation

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -5,7 +5,20 @@ import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker"
 export const frames = createFrames({
   basePath: "/frames",
   middleware: [
-    farcasterHubContext(),
+    farcasterHubContext({
+      ...(process.env.NODE_ENV === "production"
+        ? {
+            hubHttpUrl: "https://hubs.airstack.xyz",
+            hubRequestOptions: {
+              headers: {
+                "x-airstack-hubs": process.env.AIRSTACK_API_KEY as string,
+              },
+            },
+          }
+        : {
+            hubHttpUrl: "http://localhost:3010/hub",
+          }),
+    }),
     imagesWorkerMiddleware({
       imagesRoute: "/images"
     }),
